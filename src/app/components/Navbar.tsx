f4ac8b37710f46397c8c100ad92163bc632ab4f3
Yiru <caiyiru0220@gmail.com>
2025-05-16 13:47:52 +0800

    update some hospital

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -39,14 +39,14 @@ export default function Navbar() {
           </div>
 
           {/* 手機選單按鈕 */}
-          <button
+          {/* <button
             onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
             className="mobile-menu-button md:hidden"
           >
             <svg className="w-6 h-6 text-mintdark" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
             </svg>
-          </button>
+          </button> */}
         </div>
       </div>
 
